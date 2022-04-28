5ad7b2e5c734e455e0c13d0df9acd3ac33a64939
mariano <mcabezas@eia.udg.edu>
2022-04-29 08:21:47 +1000

    - accuracy prints

--- model/agem.py
+++ model/agem.py
@@ -20,7 +20,7 @@ def accuracy(output, target, topk=(1, )):
     """Computes the precision@k for the specified values of k"""
     maxk = max(topk)
     batch_size = target.size(0)
-
+    print(maxk, output.shape)
     _, pred = output.topk(maxk, 1, True, True)
     pred = pred.t()
     correct = pred.eq(target.view(1, -1).expand_as(pred))
